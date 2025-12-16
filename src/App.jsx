8c4bb1d282b9ce8db5be16cd963072f321c43ef5
23120205 <ngogiaan11111@gmail.com>
2025-12-16 11:39:53 +0700

    finish layout and finish project

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import { Outlet } from "react-router-dom";
 import NavBar from "@/components/layout/NavBar";
-import { Toaster } from "./components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner";
 
 const App = () => {
   return (
