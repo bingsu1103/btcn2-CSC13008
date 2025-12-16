b24f012121a605a5c2fad72d29db21c18614a59b
23120205 <ngogiaan11111@gmail.com>
2025-12-16 10:01:45 +0700

    implement api add favorite film to list

--- src/services/apiUser.js
+++ src/services/apiUser.js
@@ -39,6 +39,26 @@ const apiUser = {
 
     return response.json();
   },
+  addFavoriteMovie: async (id) => {
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const appToken = import.meta.env.VITE_X_APP_TOKEN;
+    const accessToken = localStorage.getItem("accessToken");
+
+    const response = await fetch(`${backendUrl}/api/users/favorites/${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-app-token": appToken,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Add to list favorite film failed");
+    }
+
+    return response.json();
+  },
 };
 
 export default apiUser;
